9ff1ceff4656177e041fcbcba30d1e45072abcd6
Aarit Jayaswal <aaritrj@gmail.com>
2024-12-14 11:28:11 -0800

    fnishing the array thing how to do it

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _03_More_Algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -39,6 +40,59 @@ public class Algorithms {
     }
     
     public static int findBrokenEgg(List<String> eggs) {
+    	for(int i = 0; i<eggs.size(); i++) {
+    		if(eggs.get(i) .equals("cracked")) {
+    			return i;
+    		}
+    	}
         return 0;
+    }
+    public static int countPearls(List<Boolean> oysters) {
+    	int count = 0;
+    	for(int i = 0; i < oysters.size(); i++) {
+    		if(oysters.get(i)) {
+    			count +=1;
+    		}
+    	}
+    	return count;
+    }
+    public static double findTallest(List<Double> peeps) {
+    	double talest = 0; 
+    	for(int i = 0; i < peeps.size(); i++) {
+    		if(peeps.get(i) > talest) {
+    			talest = peeps.get(i);
+    		}
+    	}
+    	return talest;
+    }
+    public static String findLongestWord(List<String> words) {
+    	String longest = " ";
+    	for(String i: words) {
+    		if(i.length() > longest.length()) {
+    			longest = i;
+    		}
+    	}
+    	return longest; 
+    }
+    public static boolean containsSOS(List<String>message) {
+    	for(int i = 0; i < message.size(); i++) {
+    		if(message.get(i) .contains("... --- ...")) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    public static double sortScores(List<Double> results) {
+    	ArrayList<Double> scores = new ArrayList();
+    	for(int i = 0; i < results.size(); i++) {
+    		if(results.get(i) > results.get(i+1)) {
+    			double s = results.get(i+1);
+    			double j = results.get(i);
+    			results.set(i+1, s);
+    			results.set(i, j);
+    			
+    			
+    		}
+    	}
     }
 }
